2cc2381bc3b60cd67bb62ae0c855c79d67638dfa
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2021-03-02 19:44:34 +0300

    Добавлена возможность соединения в соотвествии с документацией Google

--- app/src/main/java/com/example/bluetoothjava/BlueInfo.java
+++ app/src/main/java/com/example/bluetoothjava/BlueInfo.java
@@ -2,16 +2,33 @@ package com.example.bluetoothjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class BlueInfo extends AppCompatActivity {
+    //Сокет, с помощью которого мы будем отправлять данные на Arduino
+    BluetoothSocket clientSocket;
+
+    ConnectThread myConnect;
+
 
     public static final MyBlue EXTRA_DRINKID = new MyBlue("", "");
     public static final String EXTRA_BLUENAME = "name";
     public static final String EXTRA_BLUEADDRESS = "address";
     public static final String EXTRA_BLUETYPE = "type";
+
+    String blueAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +36,7 @@ public class BlueInfo extends AppCompatActivity {
         setContentView(R.layout.activity_blue_info);
 
         String blueName = (String) getIntent().getExtras().get(EXTRA_BLUENAME);
-        String blueAddress = (String) getIntent().getExtras().get(EXTRA_BLUEADDRESS);
+        blueAddress = (String) getIntent().getExtras().get(EXTRA_BLUEADDRESS);
         int blueType = (int) getIntent().getExtras().get(EXTRA_BLUETYPE);
         String blueTypeStr = String.valueOf(blueType);
         //MyBlue myBlue = getIntent().getExtras().get
@@ -37,5 +54,54 @@ public class BlueInfo extends AppCompatActivity {
         type.setText(blueTypeStr);
 
 
+    }
+
+    public void btnConnect(View view) {
+        //Мы хотим использовать тот bluetooth-адаптер, который задается по умолчанию
+        BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+
+        //Пытаемся проделать эти действия
+        try{
+            //Устройство с данным адресом - наш Bluetooth Bee
+            //Адрес опредеяется следующим образом: установите соединение
+            //между ПК и модулем (пин: 1234), а затем посмотрите в настройках
+            //соединения адрес модуля. Скорее всего он будет аналогичным.
+            BluetoothDevice device = bluetooth.getRemoteDevice(blueAddress);
+
+            //Инициируем соединение с устройством
+            Method m = device.getClass().getMethod(
+                    "createRfcommSocket", new Class[] {int.class});
+
+            clientSocket = (BluetoothSocket) m.invoke(device, 1);
+            clientSocket.connect();
+
+            //В случае появления любых ошибок, выводим в лог сообщение
+        } catch (IOException e) {
+            Log.d("BLUETOOTH_", e.getMessage());
+        } catch (SecurityException e) {
+            Log.d("BLUETOOTH_", e.getMessage());
+        } catch (NoSuchMethodException e) {
+            Log.d("BLUETOOTH_", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            Log.d("BLUETOOTH_", e.getMessage());
+        } catch (IllegalAccessException e) {
+            Log.d("BLUETOOTH_", e.getMessage());
+        } catch (InvocationTargetException e) {
+            Log.d("BLUETOOTH_", e.getMessage());
+        }
+
+        //Выводим сообщение об успешном подключении
+        Toast.makeText(getApplicationContext(), "CONNECTED", Toast.LENGTH_LONG).show();
+    }
+
+    public void connectMine(View view) {
+        myConnect = new ConnectThread(blueAddress);
+        myConnect.run();
+    }
+
+    @Override
+    protected void onDestroy() {
+        myConnect.cancel();
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/example/bluetoothjava/ConnectThread.java
@@ -0,0 +1,65 @@
+package com.example.bluetoothjava;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ConnectThread extends Thread {
+    public static String TAG = "BLUETOOTH";
+    private static final UUID MY_UUID =UUID.fromString ("00001101-0000-1000-8000-00805F9B34FB");
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private final BluetoothSocket mmSocket;
+    private final BluetoothDevice mmDevice;
+
+    public ConnectThread (String address) {
+        // Use a temporary object that is later assigned to mmSocket
+        // because mmSocket is final.
+        BluetoothSocket tmp = null;
+        mmDevice = bluetoothAdapter.getRemoteDevice(address);
+
+        try {
+            // Get a BluetoothSocket to connect with the given BluetoothDevice.
+            // MY_UUID is the app's UUID string, also used in the server code.
+            tmp = mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            Log.e(TAG, "Socket's create() method failed", e);
+        }
+        mmSocket = tmp;
+    }
+
+    public void run() {
+        // Cancel discovery because it otherwise slows down the connection.
+        BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+
+        try {
+            // Connect to the remote device through the socket. This call blocks
+            // until it succeeds or throws an exception.
+            mmSocket.connect();
+        } catch (IOException connectException) {
+            // Unable to connect; close the socket and return.
+            try {
+                mmSocket.close();
+            } catch (IOException closeException) {
+                Log.e(TAG, "Could not close the client socket", closeException);
+            }
+            return;
+        }
+
+        // The connection attempt succeeded. Perform work associated with
+        // the connection in a separate thread.
+        //manageMyConnectedSocket(mmSocket);
+    }
+
+    // Closes the client socket and causes the thread to finish.
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not close the client socket", e);
+        }
+    }
+}
